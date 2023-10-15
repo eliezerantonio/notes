10193e915490fe64a3bd38f4a39b028dff4f61b6
eliezerantonio <eliezerfernandoantonio@gmail.com>
2023-10-15 10:29:56 +0100

    feat: injection dependency for notes implementation

--- lib/presentation/providers/notes/notes_repository_provider.dart
+++ lib/presentation/providers/notes/notes_repository_provider.dart
@@ -1,7 +1,7 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:notes/domain/domain.dart';
 import 'package:notes/infrastructure/datasources/notes_datasource_impl.dart';
-import 'package:notes/infrastructure/repositories/notes_datasource_impl.dart';
+import 'package:notes/infrastructure/repositories/notes_repository_impl.dart';
 
 final notesRepositoryProvider = Provider<NotesRepositories>((ref) {
   final notesRespository = NoteRepositoryImpl(NotesDatasourceImpl());
