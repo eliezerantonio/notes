dbe4b16b420cb3501c6aa17f7107ffb98ef6bb13
eliezerantonio <eliezerfernandoantonio@gmail.com>
2023-10-15 10:29:01 +0100

    feat(route): implementation routes

--- lib/config/router/app_router.dart
+++ lib/config/router/app_router.dart
@@ -1,14 +1,24 @@
 import 'package:go_router/go_router.dart';
-import 'package:notes/presentation/screens/home_screen.dart';
+import 'package:notes/domain/domain.dart';
+import 'package:notes/presentation/screens/home_screen/home_screen.dart';
+import 'package:notes/presentation/screens/note_screen/note_screen.dart';
 
 final appRouter = GoRouter(
-  initialLocation: "/home",
+  initialLocation: "/",
   routes: [
     GoRoute(
-      path: "/home",
-      builder: (_, __) {
-        return HomeScreen();
+        path: "/",
+        builder: (_, __) {
+          return const HomeScreen();
+        },
+        routes: const []),
+    GoRoute(
+      path: "/edit-note",
+      builder: (_, state) {
+        final noteEntity =
+            state.extra != null ? state.extra as NoteEntity : null;
+        return NoteScreen(noteEntity);
       },
-    )
+    ),
   ],
 );
