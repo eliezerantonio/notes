ff04a0f313f1d8327dcf9f099b473612a0742000
eliezerantonio <eliezerfernandoantonio@gmail.com>
2023-11-04 19:05:41 +0100

    firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:notes/firebase_options.dart';
 
